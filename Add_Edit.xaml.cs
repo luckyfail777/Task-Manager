2febfc473f8dd9958ee372907cf835b1aa54b4a7
luckyfail777 <115218930+luckyfail777@users.noreply.github.com>
2023-11-25 18:51:59 +0400

    Add files via upload

---
+++ Add_Edit.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using TaskManager.DB;
+
+namespace TaskManager
+{
+    /// <summary>
+    /// Логика взаимодействия для Add_Edit.xaml
+    /// </summary>
+    public partial class Add_Edit : Window
+    {
+        int _id = -1;
+        public Add_Edit(int id)
+        {
+            InitializeComponent();
+            _id = id;
+            if (id > -1)
+            {
+                Output();
+            }
+        }
+
+        private void Output()
+        {
+            using (MainContext db = new MainContext())
+            {
+                DB.Task editedTask = db.Tasks.FirstOrDefault(x => x.Id == _id);
+                taskName.Text = editedTask.Name;
+                taskDescription.Text = editedTask.Description;
+                taskPriority.Text = editedTask.Priority;
+                dateChoose.SelectedDate = editedTask.DeadlineDate;
+            }
+        }
+
+        private void SaveChanges_Click(object sender, RoutedEventArgs e)
+        {
+            if (_id == -1 && dateChoose.SelectedDate != null)
+            {
+                MessageBoxResult result = MessageBox.Show("Добавить задачу?", "Подтверждение", MessageBoxButton.YesNo);
+                if (result == MessageBoxResult.Yes)
+                {
+                    using (MainContext db = new MainContext())
+                    {
+                        DB.Task newTask = new DB.Task(taskName.Text, taskDescription.Text, DateTime.Now, (DateTime)dateChoose.SelectedDate, taskPriority.Text, 0);
+                        db.Tasks.Add(newTask);
+                        db.SaveChanges();
+                    }
+
+                    MessageBox.Show("Задача успешно добавлена", "Уведомление");
+                    this.DialogResult = true;
+                    this.Close();
+                }
+            }
+            else if (dateChoose.SelectedDate != null)
+            {
+                MessageBoxResult result = MessageBox.Show("Изменить задачу?", "Подтверждение", MessageBoxButton.YesNo);
+                if (result == MessageBoxResult.Yes)
+                {
+                    using (MainContext db = new MainContext())
+                    {
+                        DB.Task newTask = new DB.Task(taskName.Text, taskDescription.Text, DateTime.Now, (DateTime)dateChoose.SelectedDate, taskPriority.Text, 0);
+                        DB.Task editedTask = db.Tasks.FirstOrDefault(x => x.Id == _id);
+                        if (editedTask != null)
+                        {
+                            editedTask.Name = taskName.Text;
+                            editedTask.Description = taskDescription.Text;
+                            editedTask.DeadlineDate = (DateTime)dateChoose.SelectedDate;
+                            editedTask.Priority = taskPriority.Text;
+                            db.SaveChanges();
+                        }
+                    }
+
+                    this.DialogResult = true;
+                    this.Close();
+                    MessageBox.Show("Изменения сохранены", "Уведомление");
+                }
+            }
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using TaskManager.DB;
+
+namespace TaskManager
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Refresh();
+        }
+
+        private void AddTask_Click(object sender, RoutedEventArgs e)
+        {
+            int id = -1;
+            Add_Edit editTask = new Add_Edit(id);
+            if (editTask.ShowDialog() == true)
+            {
+                Refresh();
+            }
+        }
+
+        private void Refresh()
+        {
+            ToDo.ItemsSource = null;
+            InProgress.ItemsSource = null;
+            Done.ItemsSource = null;
+
+            List<DB.Task> toDoList = new List<DB.Task>();
+            List<DB.Task> WIPList = new List<DB.Task>();
+            List<DB.Task> doneList = new List<DB.Task>();
+
+            using (MainContext db = new MainContext())
+            {
+                var tasks = db.Tasks.ToList();
+
+                for (int ind = 0; ind < tasks.Count; ind++)
+                {
+                    if (tasks[ind].Status == 0)
+                    {
+                        toDoList.Add(tasks[ind]);
+                    }
+                    else if (tasks[ind].Status == 1)
+                    {
+                        WIPList.Add(tasks[ind]);
+                    }
+                    else if (tasks[ind].Status == 2)
+                    {
+                        doneList.Add(tasks[ind]);
+                    }
+                }
+            }
+
+            ToDo.ItemsSource = toDoList;
+            InProgress.ItemsSource = WIPList;
+            Done.ItemsSource = doneList;
+        }
+
+        private void EditTask_Click(object sender, RoutedEventArgs e)
+        {
+            var tag = int.TryParse(((Button)sender).Tag.ToString(), out int id);
+
+            Add_Edit editTask = new Add_Edit(id);
+            if (editTask.ShowDialog() == true)
+            {
+                Refresh();
+            }
+        }
+
+        private void DeleteTask_Click(object sender, RoutedEventArgs e)
+        {
+            var tag = int.TryParse(((Button)sender).Tag.ToString(), out int id);
+
+            using (MainContext db = new MainContext())
+            {
+                DB.Task task = db.Tasks.FirstOrDefault(x => x.Id == id);
+                if (task != null)
+                {
+                    db.Tasks.Remove(task);
+                    db.SaveChanges();
+                }
+
+                Refresh();
+            }
+        }
+
+        private void ToWIP_Click(object sender, RoutedEventArgs e)
+        {
+            var tag = int.TryParse(((Button)sender).Tag.ToString(), out int id);
+
+            using (MainContext db = new MainContext())
+            {
+                DB.Task task = db.Tasks.FirstOrDefault(x => x.Id == id);
+                if (task != null)
+                {
+                    task.Status = 1;
+                    db.SaveChanges();
+                }
+
+                Refresh();
+            }
+        }
+
+        private void ToDone_Click(object sender, RoutedEventArgs e)
+        {
+            var tag = int.TryParse(((Button)sender).Tag.ToString(), out int id);
+
+            using (MainContext db = new MainContext())
+            {
+                DB.Task task = db.Tasks.FirstOrDefault(x => x.Id == id);
+                if (task != null)
+                {
+                    task.Status = 2;
+                    db.SaveChanges();
+                }
+
+                Refresh();
+            }
+        }
+
+        private void searchBar_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (searchBar.Text != "")
+            {
+                ToDo.ItemsSource = null;
+                InProgress.ItemsSource = null;
+                Done.ItemsSource = null;
+
+                List<DB.Task> toDoList = new List<DB.Task>();
+                List<DB.Task> WIPList = new List<DB.Task>();
+                List<DB.Task> doneList = new List<DB.Task>();
+
+                using (MainContext db = new MainContext())
+                {
+                    var tasks = db.Tasks.ToList();
+                    tasks = tasks.Where<DB.Task>(x => x.Name.Contains(searchBar.Text)).ToList();
+
+                    for (int ind = 0; ind < tasks.Count; ind++)
+                    {
+                        if (tasks[ind].Status == 0)
+                        {
+                            toDoList.Add(tasks[ind]);
+                        }
+                        else if (tasks[ind].Status == 1)
+                        {
+                            WIPList.Add(tasks[ind]);
+                        }
+                        else if (tasks[ind].Status == 2)
+                        {
+                            doneList.Add(tasks[ind]);
+                        }
+                    }
+                }
+
+                ToDo.ItemsSource = toDoList;
+                InProgress.ItemsSource = WIPList;
+                Done.ItemsSource = doneList;
+            }
+            else
+            {
+                Refresh();
+            }
+        }
+    }
+}
